495fd89f404f6c70daf0b1e815eb4f1f64e8ef16
jmmarao <jmmarao@gmail.com>
2022-07-20 11:36:32 -0300

    [TASK-1] Math operations

---
+++ 0x00/calculadora/OperacaoBasica.java
@@ -0,0 +1,13 @@
+public class OperacaoBasica {
+    public static double somar(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtrair(double a, double b) {
+        return a - b;
+    }    public static double multiplicar(double a, double b) {
+        return a * b;
+    }    public static double diividir(double a, double b) {
+        return a / b;
+    }
+}
